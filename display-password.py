0cbac58dab313787dd403471852073667bed6cd6
nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>
2024-02-08 02:04:08 -0300

    feat(*potfile): All potfiles are welcome!
    Also add support for the Waveshare 1.54 inch V2
    
    Signed-off-by: nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>

--- display-password.py
+++ display-password.py
@@ -2,6 +2,7 @@ from pwnagotchi.ui.components import LabeledValue
 from pwnagotchi.ui.view import BLACK
 import pwnagotchi.ui.fonts as fonts
 import pwnagotchi.plugins as plugins
+from time import sleep
 import logging
 import os
 
@@ -31,6 +32,9 @@ class DisplayPassword(plugins.Plugin):
         elif ui.is_waveshare27inch():
             h_pos = (0, 153)
             v_pos = (216, 122)
+        elif ui.is_waveshare1in54V2():
+            h_pos = (0, 92)
+            v_pos = (78, 67)
         else:
             h_pos = (0, 91)
             v_pos = (180, 61)
@@ -66,5 +70,9 @@ class DisplayPassword(plugins.Plugin):
             ui.remove_element("display-password")
 
     def on_ui_update(self, ui):
-        last_line = "tail -n 1 /root/handshakes/wpa-sec.cracked.potfile | awk -F: '{print $3 \" - \" $4}'"
-        ui.set("display-password", "%s" % (os.popen(last_line).read().rstrip()))
+        for file in os.listdir('/root/handshakes'):
+            if file.endswith(".potfile"):
+                with open('/root/handshakes/*.potfile', 'r') as file:
+                    lines = file.readlines()
+                    last_line = lines[-1].split(":")[2:]
+                    ui.set("display-password", f"{last_line}")
