a52b1540904aaf0ab8fd8110d523dbe948fbe8be
nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>
2024-02-09 22:03:13 -0300

    feat(installation script): Add the value, key of custom plugins on config.toml if not exists
    Signed-off-by: nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 CONFIG_FILE="/etc/pwnagotchi/config.toml"
+CONFIG_FILE="config.toml"
 
 function user_sleep() {
 	sleep 0.5
@@ -11,9 +14,9 @@ function check_toml_key_exists() {
 	local config_file="$2"
 
 	if grep -q "^${key}" "$config_file"; then
-		echo "The '$key' already exists on $config_file."
+		echo "[ + ] The '$key' already exists on $config_file."
 	else
-		echo "Creating '$key' on $config_file."
+		echo "[ ~ ] Creating '$key' on $config_file."
 		echo "${key} = true " >>"$config_file"
 	fi
 }
@@ -47,6 +50,20 @@ function modify_config_files() {
 	edit_configuration_values "main.plugins.display-password.orientation" "$orientation" "$CONFIG_FILE"
 }
 
+function get_installation_path() {
+	check_toml_key_exists "main.custom_plugins" "$CONFIG_FILE"
+	installation_dir=$(awk '/^main.custom_plugins = / {print $3}' "$CONFIG_FILE")
+	if [ -z "${installation_dir//\"/}" ] || [ "$installation_dir" = true ]; then
+		echo "[ ! ] The installation directory was not found in the configuration file"
+		read -r -p "Please enter the installation directory, press Enter to set '/usr/local/share/pwnagotchi/custom-plugins' or specify yours with absolute path: " installation_dir
+	fi
+	if [ -z "${installation_dir//\"/}" ]; then
+		installation_dir="/usr/local/share/pwnagotchi/custom-plugins"
+	fi
+	edit_configuration_values "main.custom_plugins" "${installation_dir}" "$CONFIG_FILE"
+	installation_dir="${installation_dir//\"/}"
+}
+
 # Main
 
 # Check that the script is running as root
@@ -55,19 +72,10 @@ if [ "$EUID" -ne 0 ]; then
 	echo "[ ! ] This script need to be run as root"
 	exit 0
 fi
-
-installation_dir=$(awk '/^main.custom_plugins = / {print $3}' "$CONFIG_FILE")
-if [ -z "${installation_dir//\"}" ]; then
-	echo "[ ! ] The installation directory was not found in the configuration file"
-	read -r -p "Please enter the installation directory: [/usr/local/share/pwnagotchi/custom-plugins]" installation_dir
-fi
-if [ -z "${installation_dir//\"}" ]; then
-	installation_dir="/usr/local/share/pwnagotchi/custom-plugins"
-fi
-installation_dir=${installation_dir//\"/}
+echo "[ + ] Getting installation path..."
+get_installation_path
 echo "[ + ] Creating symbolic link to ${installation_dir}"
 ln -sf "$(pwd)/display-password.py" "${installation_dir}/display-password.py"
-
 echo "[ + ] Backing up configuration files..."
 cp "${CONFIG_FILE}" "${CONFIG_FILE}.bak"
 read -r -p "Do you want the horizontal or vertical orientation? [H/v] " orientation
