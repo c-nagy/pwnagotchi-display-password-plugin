e2cca6e35718cd0c5d15e6d99d2da53cf471a126
nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>
2024-02-09 21:01:54 -0300

    refactor(Try, except, logs, position): Added logs, and some try except blocks for traceability
    Also change a little the position for the 1.54inch display
    
    Signed-off-by: nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>

--- display-password.py
+++ display-password.py
@@ -2,7 +2,6 @@ from pwnagotchi.ui.components import LabeledValue
 from pwnagotchi.ui.view import BLACK
 import pwnagotchi.ui.fonts as fonts
 import pwnagotchi.plugins as plugins
-from time import sleep
 import logging
 import os
 
@@ -17,62 +16,74 @@ class DisplayPassword(plugins.Plugin):
         logging.info("display-password loaded")
 
     def on_ui_setup(self, ui):
-        if ui.is_waveshare_v2():
-            h_pos = (0, 95)
-            v_pos = (180, 61)
-        elif ui.is_waveshare_v1():
-            h_pos = (0, 95)
-            v_pos = (170, 61)
-        elif ui.is_waveshare144lcd():
-            h_pos = (0, 92)
-            v_pos = (78, 67)
-        elif ui.is_inky():
-            h_pos = (0, 83)
-            v_pos = (165, 54)
-        elif ui.is_waveshare27inch():
-            h_pos = (0, 153)
-            v_pos = (216, 122)
-        elif ui.is_waveshare1in54V2():
-            h_pos = (0, 92)
-            v_pos = (78, 67)
-        else:
-            h_pos = (0, 91)
-            v_pos = (180, 61)
+        try:
+            if ui.is_waveshare_v2():
+                h_pos = (0, 95)
+                v_pos = (180, 61)
+            elif ui.is_waveshare_v1():
+                h_pos = (0, 95)
+                v_pos = (170, 61)
+            elif ui.is_waveshare144lcd():
+                h_pos = (0, 92)
+                v_pos = (78, 67)
+            elif ui.is_inky():
+                h_pos = (0, 83)
+                v_pos = (165, 54)
+            elif ui.is_waveshare2in7():
+                h_pos = (0, 153)
+                v_pos = (216, 122)
+            elif ui.is_waveshare1in54V2():
+                h_pos = (0, 92)
+                v_pos = (70, 170)
+            else:
+                h_pos = (0, 91)
+                v_pos = (180, 61)
 
-        if self.options["orientation"] == "vertical":
-            ui.add_element(
-                "display-password",
-                LabeledValue(
-                    color=BLACK,
-                    label="",
-                    value="",
-                    position=v_pos,
-                    label_font=fonts.Bold,
-                    text_font=fonts.Small,
-                ),
-            )
-        else:
-            # default to horizontal
-            ui.add_element(
-                "display-password",
-                LabeledValue(
-                    color=BLACK,
-                    label="",
-                    value="",
-                    position=h_pos,
-                    label_font=fonts.Bold,
-                    text_font=fonts.Small,
-                ),
-            )
+            if self.options["orientation"] == "vertical":
+                ui.add_element(
+                    "display-password",
+                    LabeledValue(
+                        color=BLACK,
+                        label="",
+                        value="",
+                        position=v_pos,
+                        label_font=fonts.Bold,
+                        text_font=fonts.Small,
+                    ),
+                )
+            else:
+                # default to horizontal
+                ui.add_element(
+                    "display-password",
+                    LabeledValue(
+                        color=BLACK,
+                        label="",
+                        value="",
+                        position=h_pos,
+                        label_font=fonts.Bold,
+                        text_font=fonts.Small,
+                    ),
+                )
+        except Exception as e:
+            logging.error(f"[DISPLAY-PASSWORD] {e}")
 
     def on_unload(self, ui):
-        with ui._lock:
-            ui.remove_element("display-password")
+        try:
+            with ui._lock:
+                ui.remove_element("display-password")
+        except Exception as e:
+            logging.error(f"[DISPLAY-PASSWORD] {e}")
 
     def on_ui_update(self, ui):
-        for file in os.listdir('/root/handshakes'):
-            if file.endswith(".potfile"):
-                with open('/root/handshakes/*.potfile', 'r') as file:
-                    lines = file.readlines()
-                    last_line = lines[-1].split(":")[2:]
-                    ui.set("display-password", f"{last_line}")
+        logging.debug("[DISPLAY-PASSWORD] Actualizando UI")
+        try:
+            for file in os.listdir("/root/handshakes"):
+                if file.endswith(".potfile"):
+                    with open(f'/root/handshakes/{file}', 'r') as file:
+                        lines = file.readlines()
+                        if len(lines) > 0:
+                            last_line = lines[-1].split(":")[2:]
+                            last_line = ":".join(last_line)
+                            ui.set("display-password", f"{last_line}")
+        except Exception as e:
+            logging.error(f"[DISPLAY-PASSWORD] {e}")
